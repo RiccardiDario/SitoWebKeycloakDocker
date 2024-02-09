39e7c6fd053ddd85f9b9cd2b7f55e81b6546b3ac
RiccardiDario <riccardi94dario@gmail.com>
2024-02-09 11:06:41 +0100

    update

--- src/Routes/PrivateRoute.tsx
+++ src/Routes/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import type { LoaderFunctionArgs } from "react-router-dom";
 
 function PrivateRoute({ request }: LoaderFunctionArgs) {
   if (!kc.authenticated) {
-    const options = {redirectUri: 'http://localhost:3000/',};
+    const options = {redirectUri: 'http://localhost:8080/',};
     kc.login(options);
   }
 return null;
